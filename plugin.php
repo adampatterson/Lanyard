2df3456beaf5642d612c7903cce0d29f32b03da9
Adam Patterson <hello@adampatterson.ca>
2021-03-08 17:30:57 -0700

    .

--- plugin.php
+++ plugin.php
@@ -17,6 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once __DIR__ . '/vendor/autoload.php';
 //add_action( 'plugins_loaded', array( 'PluginNameSpace\Bootstrap', 'init' ) );
+
 PluginNameSpace\Bootstrap::init();
 
 register_activation_hook( __FILE__, [ 'PluginNameSpace\Actions', 'activationHook' ] );
